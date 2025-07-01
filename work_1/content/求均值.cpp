9dfcb3be9c61f9a416bc2c6b9012e4886fbc3479
unknown <2787888511@qq.com@gmail.com>
2025-07-01 19:38:47 +0800

    first work -lzh-927

---
+++ work_1/content/求均值.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main()
+{
+	FILE*pf = fopen("input.txt","w");
+	srand((unsigned int)time(NULL));
+	int j=0;
+	for(j=0;j<5;j++)
+	{
+		int num=rand() %100;
+		fprintf(pf,"%d ",num);
+	}
+	fclose(pf);
+	double sum=0;
+	int count=5;
+	float ch[100];
+	FILE*pf_read = fopen("input.txt","r");
+	int i=0;
+	for(i=0;i<count;i++)
+	{
+		fscanf(pf_read,"%f",&ch[i]);
+		sum+=ch[i];
+	}
+	fclose(pf_read);
+	double avg=sum/count;
+	FILE* pf2 = fopen("output.txt","w");
+	fprintf(pf2,"%f",avg);
+	fclose(pf2);
+	return 0;
+}
+
